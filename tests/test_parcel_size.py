da35b01e29c9671787d2040a2fcf3f810c547dfd
Lei Ferretti <elisa.c.ferretti@gmail.com>
2023-05-29 13:01:30 +0100

    more tests

--- tests/test_parcel_size.py
+++ tests/test_parcel_size.py
@@ -14,3 +14,42 @@ def test_small_parcel():
 
     parcel = Parcel(width=1, height=5, depth=10)
     assert calculate_parcel_size(parcel) != ParcelSize.SMALL
+
+
+def test_medium_parcel():
+    parcel = Parcel(width=9, height=9, depth=9)
+    assert calculate_parcel_size(parcel) != ParcelSize.MEDIUM
+
+    parcel = Parcel(width=10, height=10, depth=10)
+    assert calculate_parcel_size(parcel) == ParcelSize.MEDIUM
+
+    parcel = Parcel(width=49, height=49, depth=49)
+    assert calculate_parcel_size(parcel) == ParcelSize.MEDIUM
+
+    parcel = Parcel(width=49, height=5, depth=50)
+    assert calculate_parcel_size(parcel) != ParcelSize.MEDIUM
+
+
+def test_large_parcel():
+    parcel = Parcel(width=49, height=49, depth=49)
+    assert calculate_parcel_size(parcel) != ParcelSize.LARGE
+
+    parcel = Parcel(width=50, height=50, depth=50)
+    assert calculate_parcel_size(parcel) == ParcelSize.LARGE
+
+    parcel = Parcel(width=99, height=99, depth=99)
+    assert calculate_parcel_size(parcel) == ParcelSize.LARGE
+
+    parcel = Parcel(width=99, height=5, depth=100)
+    assert calculate_parcel_size(parcel) != ParcelSize.LARGE
+
+
+def test_extra_large_parcel():
+    parcel = Parcel(width=99, height=99, depth=99)
+    assert calculate_parcel_size(parcel) != ParcelSize.EXTRA_LARGE
+
+    parcel = Parcel(width=100, height=100, depth=100)
+    assert calculate_parcel_size(parcel) == ParcelSize.EXTRA_LARGE
+
+    parcel = Parcel(width=9999, height=99999, depth=9999999999)
+    assert calculate_parcel_size(parcel) == ParcelSize.EXTRA_LARGE
